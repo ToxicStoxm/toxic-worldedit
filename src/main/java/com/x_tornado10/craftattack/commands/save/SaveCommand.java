5aa5bb29376c04f56e7d7da8f99c184fee7709da
ToxicStoxm <dominik0815oberlechner@gmail.com>
2023-12-12 20:32:01 +0100

    commit

--- src/main/java/com/x_tornado10/craftattack/commands/save/SaveCommand.java
+++ src/main/java/com/x_tornado10/craftattack/commands/save/SaveCommand.java
@@ -1,4 +1,14 @@
 package com.x_tornado10.craftattack.commands.save;
 
-public class SaveCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class SaveCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NonNull CommandSender commandSender,@NonNull Command command,@NonNull String s,@NonNull String[] strings) {
+        return false;
+    }
 }
